9230e93e056f911949e9f9323679a9876176eaa6
HallamSaunders <hals4072@gmail.com>
2025-07-25 09:40:08 +0100

    Updated file structure, created type system for logs.

---
+++ logs/logs.go
@@ -0,0 +1,34 @@
+package logs
+
+// Types based on OpenTelemetry's spec for the logs produced by the file exporter
+type LogFile struct {
+	ResourceLogs []struct {
+		Resource struct {
+			Attributes []Attribute `json:"attributes"`
+		} `json:"resource"`
+		ScopeLogs []struct {
+			LogRecords []LogRecord `json:"logRecords"`
+		} `json:"scopeLogs"`
+	} `json:"resourceLogs"`
+}
+
+type LogRecord struct {
+	TimeUnixNano           int64       `json:"timeUnixNano"`
+	SeverityNumber         int         `json:"severityNumber"`
+	SeverityText           string      `json:"severityText"`
+	Body                   ValueField  `json:"body"`
+	Attributes             []Attribute `json:"attributes"`
+	DroppedAttributesCount int         `json:"droppedAttributesCount,omitempty"`
+	TraceID                string      `json:"traceId,omitempty"`
+	SpanID                 string      `json:"spanId,omitempty"`
+}
+
+type Attribute struct {
+	Key   string     `json:"key"`
+	Value ValueField `json:"value"`
+}
+
+type ValueField struct {
+	// Values of a log could possibly be of various types but for simplicity, let's assume strings for now
+	StringValue string `json:"stringValue,omitempty"`
+}
